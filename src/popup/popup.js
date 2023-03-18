ea1caa99ccb0939351526c7f0553796dcbe62d49
99mini <itman390@gmail.com>
2023-03-18 21:40:01 +0900

    add: 나무 이미지 추가

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,17 +1,43 @@
 const counter = document.getElementById("counter");
 getStorage();
+setIcons(Number(counter.innerHTML));
 
 function incrementCount() {
-  chrome.storage.local.set({ count: Number(counter.innerHTML) + 1 });
+  setStorage(Number(counter.innerHTML) + 1);
+  // chrome.storage.local.set({ count: Number(counter.innerHTML) + 1 });
   getStorage();
+  setIcons(Number(counter.innerHTML));
+}
+
+function clearCount() {
+  setStorage(0);
+  getStorage();
+
+  document.getElementById("plantImg").src =
+    "../assets/images/plant/plant00.png";
+  chrome.action.setIcon({ path: "../assets/images/plant/plant00.png" });
+}
+
+function setStorage(data) {
+  chrome.storage.local.set({ count: data });
 }
 
 function getStorage() {
   chrome.storage.local.get("count", function (result) {
     if (result.count) {
       counter.innerHTML = result.count;
+    } else {
+      counter.innerHTML = 0;
     }
   });
+}
+
+function setIcons(number) {
+  if (number >= 5) {
+    document.getElementById("plantImg").src =
+      "../assets/images/plant/plant16.png";
+    chrome.action.setIcon({ path: "../assets/images/plant/plant16.png" });
+  }
 }
 
 document
@@ -19,3 +45,7 @@ document
   .addEventListener("click", function (event) {
     incrementCount();
   });
+
+document.getElementById("clearBtn").addEventListener("click", function (event) {
+  clearCount();
+});
