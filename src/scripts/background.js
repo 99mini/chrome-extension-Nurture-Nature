4745e8af5ee39803c3dabc66c3cf796ce28c7832
99mini <itman390@gmail.com>
2023-07-06 12:43:14 +0900

    refactory: 환경변수 설정, 사용하지 않는 함수 정리

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -21,8 +21,7 @@ const REFRESH_ACTION = "refreshAction";
  * } ,...]
  */
 async function fecthUserInfo(username) {
-  const baseUrl =
-    "http://chromeextensionappbe-env.eba-q3tmhahd.ap-northeast-2.elasticbeanstalk.com";
+  const baseUrl = process.env.API_URL;
   const url = "/api/user";
   const query = "?username=" + username;
   const res = await fetch(baseUrl + url + query);
@@ -52,53 +51,13 @@ function calcCommitCount(commitsByDay) {
   return commitsOfTenDays;
 }
 
-// not use
-// /**
-//  *
-//  * @param {*} username
-//  * @returns
-//  */
-// async function fecthCommitCount(username) {
-//   let commitCount = 0;
-//   let commitsByDay = {};
-//   const repoResponse = await fetch(
-//     `https://api.github.com/users/${username}/repos`
-//   );
-//   const repos = await repoResponse.json();
-//   for (const repo of repos) {
-//     const commitResponse = await fetch(
-//       // get public commit count
-//       `https://api.github.com/repos/${repo.owner.login}/${repo.name}/commits?author=${username}`
-//       // https://api.github.com/repos/99mini/pygame_study/commits?author=99mini
-//     );
-
-//     const commits = await commitResponse.json();
-//     commitCount += commits.length;
-//     commits.forEach((commit) => {
-//       const date = new Date(commit.commit.author.date)
-//         .toISOString()
-//         .split("T")[0];
-//       if (!commitsByDay[date]) {
-//         commitsByDay[date] = 0;
-//       }
-//       commitsByDay[date]++;
-//     });
-//   }
-
-//   return { commitsByDay, commitCount };
-// }
-
 /**
  *
  * @param {*} username
  */
 async function setCommitCount(username) {
-  const commitsByDay = await fecthUserInfo(username);
+  const commitsByDay = fecthUserInfo(username);
   const commitCount = calcCommitCount(commitsByDay);
-
-  // console.log(username);
-  // console.log(commitsByDay);
-  // console.log(commitCount);
 
   setStorage(COMMIT_COUNT, commitCount);
   setStorage(COMMITS_BY_DAY, commitsByDay);
@@ -129,64 +88,6 @@ async function getStorage(key) {
   let result = await newPromise;
   return result;
 }
-
-// not use
-// /**
-//  *
-//  * @param {Date} date `Date`
-//  * @returns {string} yyyy-mm-dd 형태의 string
-//  */
-// function getDateString(date) {
-//   const year = date.getFullYear().toString();
-//   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-//   const day = date.getDate().toString().padStart(2, "0");
-
-//   const dateString = `${year}-${month}-${day}`;
-//   return dateString;
-// }
-
-// not use
-// /**
-//  *
-//  * @param {number}interval
-//  * @returns {Date[]}
-//  * - 오늘부터 인터벌 기간 과거 동안의 Date 객체를 반환한다.
-//  * - 예] interval = 10, 오늘 1월 11일 => 1월 11일 ~ 1월 1일 까지의 Date 객체 반환
-//  */
-// function getDateObjList(interval) {
-//   const dates = [];
-
-//   for (let i = 0; i < interval; i++) {
-//     const date = new Date();
-//     date.setDate(date.getDate() - i);
-//     dates.push(date);
-//   }
-//   return dates;
-// }
-
-// not use
-// /**
-//  *
-//  * @param {object} commitsByDayObj
-//  * @param {number} interval
-//  * @returns {object[]} interval 기간동안의 커밋 리스트 {날짜: 개수}
-//  */
-// function formateCommitsByDay(commitsByDayObj, interval) {
-//   const dates = getDateObjList(interval);
-//   const dateStringList = dates.map((date) => getDateString(date));
-//   let returnList = [];
-//   dateStringList.map((dateString) => {
-//     let tempObj = {};
-//     if (!commitsByDayObj[dateString]) {
-//       tempObj[dateString] = 0;
-//     } else {
-//       tempObj[dateString] = commitsByDayObj[dateString];
-//     }
-//     returnList.push(tempObj);
-//   });
-
-//   return returnList.reverse();
-// }
 
 // 호출 받기
 chrome.runtime.onMessage.addListener(async function (
